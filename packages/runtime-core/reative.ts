b1e74d05a81d310d3e8051cf5e4da2883e72b768
zhouzijian <1394255600@qq.com>
2024-04-18 16:49:18 +0800

    feat: 搭建ref结构

--- packages/runtime-core/reative.ts
+++ packages/runtime-core/reative.ts
@@ -84,23 +84,3 @@ function createReactiveObject(
     return proxy;
 }
 
-function ref(value: any) {
-    if (!isObject(value)) {
-        value = { value }
-    }
-    const baseHandlers = {
-        get: function (target: object, key: string | symbol) {
-            return target[key];
-        },
-        set: function (target: object, key: string | symbol, value: unknown, receiver: object) {
-            let oldValue = target[key];
-            const result = Reflect.set(target, key, value, receiver)
-            return true;
-        }
-    }
-    const collectionHandlers = {
-    }
-    return createReactiveObject(value, false, baseHandlers, collectionHandlers, reactiveMap)
-}
-
-export default ref
